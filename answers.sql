1dd9f067b5951562a9b31f576f1ef228dbec1038
EsihleM <esihlemkhusane@gmail.com>
2025-04-19 19:00:51 +0200

    Update answers.sql

---
+++ answers.sql
@@ -0,0 +1,11 @@
+--Question 1
+SELECT 
+    OrderID,
+    CustomerName,
+    LTRIM(RTRIM(value)) AS Product
+FROM 
+    ProductDetail
+CROSS APPLY 
+    STRING_SPLIT(Products, ',');
+
+-- Question 2
